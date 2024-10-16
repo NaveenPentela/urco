a974ee654499a91e698a88f83f58fcf44e67572a
KrishPatel01 <krishpatel7191@gmail.com>
2024-10-16 08:10:28 +0530

    set azure db

--- urco/urco/settings.py
+++ urco/urco/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'urco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'flinders-app-db',
+        'USER': 'flindersdb@flinders-app-db',
+        'PASSWORD': 'chem@db@102938475',
+        'HOST': 'flinders-app-db.mysql.database.azure.com',
+        'PORT': '3306',
     }
 }
 
